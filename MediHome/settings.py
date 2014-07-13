a1d9e033e41aa693a3d2ea9a73ce45f3352c4c87
Praveen Jindal <praveenjindal62@gmail.com>
2014-07-12 22:51:30 -0700

    settings changed

--- MediHome/settings.py
+++ MediHome/settings.py
@@ -10,6 +10,7 @@ https://docs.djangoproject.com/en/dev/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import dj_database_url
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 DATABASES['default'] = dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 
